48812e917b953e67d4f7888eec8010c73ac99dda
Maxwell Sime <maxwellsime@gmail.com>
2024-10-30 22:02:36 +0000

    Remove outdated database SQL schema

--- PaymentsRepository.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE PaymentsRepository (
-    ID uuid PRIMARY KEY,
-    Authorized boolean NOT NULL,
-    Amount integer NOT NULL,
-    CardNumberLastFour integer(4) NOT NULL,
-    Currency char(3) NOT NULL,
-    ExpirationDate date NOT NULL,
-)
